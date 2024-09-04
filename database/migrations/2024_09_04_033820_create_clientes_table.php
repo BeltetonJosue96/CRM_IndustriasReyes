ea492c074b12029ec3ea24bdbd303a1505077201
BeltetonJosue96 <josuebelteton@icloud.com>
2024-09-04 07:53:43 -0600

    Clientes: migración creada y estandarización de nombre de la tabla

--- database/migrations/2024_09_04_033820_create_clientes_table.php
+++ database/migrations/2024_09_04_033820_create_clientes_table.php
@@ -11,8 +11,30 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('clientes', function (Blueprint $table) {
-            $table->id();
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id('id_cliente');
+            $table->string('nombre', 145);
+            $table->string('apellidos', 145);
+            $table->string('identificacion', 25)->unique();
+            $table->string('telefono', 45);
+            $table->unsignedBigInteger('id_empresa')->nullable();
+            $table->string('cargo', 45)->nullable();
+            $table->string('direccion', 245);
+            $table->string('referencia', 245);
+            $table->string('municipio', 45);
+            $table->unsignedBigInteger('id_departamento');
+
+            $table->foreign('id_empresa')
+                ->references('id_empresa')
+                ->on('empresa')
+                ->onDelete('set null');
+
+            $table->foreign('id_departamento')
+                ->references('id_departamento')
+                ->on('departamento')
+                ->onDelete('cascade');
+
+            $table->unique('id_cliente');
             $table->timestamps();
         });
     }
@@ -22,6 +44,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('clientes');
+        Schema::dropIfExists('cliente');
     }
 };
