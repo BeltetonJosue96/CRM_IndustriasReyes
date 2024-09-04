39435c942c31b80b0a0542bade63b2635bcafcbe
BeltetonJosue96 <josuebelteton@icloud.com>
2024-09-03 21:43:28 -0600

    Creación del modelo, migración y controlador de Control de Detalle de Checklist.

---
+++ app/Http/Controllers/DetalleCheckController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DetalleCheck;
+use Illuminate\Http\Request;
+
+class DetalleCheckController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DetalleCheck $detalleCheck)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DetalleCheck $detalleCheck)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DetalleCheck $detalleCheck)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DetalleCheck $detalleCheck)
+    {
+        //
+    }
+}
