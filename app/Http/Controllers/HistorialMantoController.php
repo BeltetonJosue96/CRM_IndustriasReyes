fac7345aa89e8bdf0d2ecf355efe80fdfbd6ccd3
BeltetonJosue96 <josuebelteton@icloud.com>
2024-09-03 21:44:12 -0600

    Creación del modelo, migración y controlador de Control de Historial de Mantenimiento.

---
+++ app/Http/Controllers/HistorialMantoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HistorialManto;
+use Illuminate\Http\Request;
+
+class HistorialMantoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HistorialManto $historialManto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(HistorialManto $historialManto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, HistorialManto $historialManto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HistorialManto $historialManto)
+    {
+        //
+    }
+}
