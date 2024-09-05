6c0142b1e420adf7177f2592531b44e375a53352
BeltetonJosue96 <josuebelteton@icloud.com>
2024-09-05 15:51:27 -0600

    Se crea Seeder para insertar los departamentos de Guatemala.

---
+++ database/seeders/DepartamentoSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DepartamentoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $departamentos = [
+            'Alta Verapaz',
+            'Baja Verapaz',
+            'Chimaltenango',
+            'Chiquimula',
+            'El Progreso',
+            'Escuintla',
+            'Guatemala',
+            'Huehuetenango',
+            'Izabal',
+            'Jalapa',
+            'Jutiapa',
+            'Petén',
+            'Quetzaltenango',
+            'Quiché',
+            'Retalhuleu',
+            'Sacatepéquez',
+            'San Marcos',
+            'Santa Rosa',
+            'Sololá',
+            'Suchitepéquez',
+            'Totonicapán',
+            'Zacapa',
+        ];
+
+        foreach ($departamentos as $nombre) {
+            DB::table('departamento')->insert([
+                'nombre' => $nombre,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
