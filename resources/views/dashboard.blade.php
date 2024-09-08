12d8518997c7ac1b05ed7a6cee566c7fe97954f3
BeltetonJosue96 <josuebelteton@icloud.com>
2024-09-07 22:24:24 -0600

    Mejora en diseño de tabla.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,35 +15,35 @@
                     <div class="grid grid-cols-2 gap-8 mt-6">
                         <a href="{{ route('clientes.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
                             <img src="https://cdn-icons-png.freepik.com/256/2194/2194222.png" alt="Clientes" class="w-20 h-20 mb-2">
-                            <span class="text-xl font-semibold text-gray-800 dark:text-gray-200">Clientes</span>
+                            <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">Clientes</span>
                         </a>
                         <a href="{{ route('checklist.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
                             <img src="https://cdn-icons-png.freepik.com/256/2666/2666436.png" alt="Checklist" class="w-20 h-20 mr-2">
-                            <span class="text-xl font-semibold text-gray-800 dark:text-gray-200">
+                            <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
                                 Checklist Posventa
                             </span>
                         </a>
                         <a href="{{ route('ventas.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
                             <img src="https://cdn-icons-png.freepik.com/256/781/781760.png" alt="Checklist" class="w-20 h-20 mr-2">
-                            <span class="text-xl font-semibold text-gray-800 dark:text-gray-200">
+                            <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
                                 Ventas
                             </span>
                         </a>
                         <a href="{{ route('historial.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
                             <img src="https://cdn-icons-png.freepik.com/256/2936/2936725.png" alt="Checklist" class="w-20 h-20 mr-2">
-                            <span class="text-xl font-semibold text-gray-800 dark:text-gray-200">
+                            <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
                                 Reportes
                             </span>
                         </a>
                         <a href="{{ route('productos.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
                             <img src="https://cdn-icons-png.freepik.com/256/2702/2702154.png" alt="Productos" class="w-20 h-20 mr-2">
-                            <span class="text-xl font-semibold text-gray-800 dark:text-gray-200">
+                            <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
                                 Catálogo de Productos
                             </span>
                         </a>
                         <a href="{{ route('planes.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
                             <img src="https://cdn-icons-png.freepik.com/256/4233/4233834.png" alt="Productos" class="w-20 h-20 mr-2">
-                            <span class="text-xl font-semibold text-gray-800 dark:text-gray-200">
+                            <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
                                 Planes de Mantenimiento
                             </span>
                         </a>
--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -4,47 +4,75 @@
             {{ __('Productos') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @if($productos->isEmpty())
-                        <p class="text-center text-gray-500">No hay productos disponibles en este momento.</p>
+                    <div class="text-center mt-6">
+                        <a href="{{ route('productos.create') }}">
+                            <x-primary-button class="ms-3">
+                                {{ __('Agregar nuevo producto') }}
+                            </x-primary-button>
+                        </a>
+                    </div>
+                    <div class="mt-6 flex justify-between items-center">
+                        <h3 class="justify-start text-xl text-gray-50 dark:text-gray-400">Lista de productos</h3>
+                        <form action="{{ route('productos.index') }}" method="GET" class="flex">
+                            <input type="text" name="search" value="{{ request('search') }}" placeholder="Buscar productos..." class="px-4 py-2 border rounded-md dark:bg-gray-700 dark:text-white dark:border-gray-600">
+                            <x-primary-button class="ms-3">
+                                Buscar/Limpiar
+                            </x-primary-button>
+                        </form>
+                    </div>
+                @if($productos->isEmpty())
+                        <p class="text-center text-gray-500 dark:text-gray-400">Sin coincidencias, no hay productos disponibles en este momento.</p>
                     @else
-                        <div class="overflow-x-auto">
-                            <table class="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
-                                <thead class="bg-gray-100 dark:bg-gray-700 text-gray-600 dark:text-gray-200">
+                        <div class="mt-6 relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
+                            <table class="w-full table-auto min-w-max divide-y divide-gray-200 dark:divide-gray-700">
+                                <thead class="bg-gray-50 dark:bg-gray-700">
                                 <tr>
-                                    <th class="py-2 px-4 border-b">ID</th>
-                                    <th class="py-2 px-4 border-b">Producto</th>
-                                    <th class="py-2 px-4 border-b">Fecha de Creación</th>
-                                    <th class="py-2 px-4 border-b">Última Actualización</th>
+                                    <th scope="col" class="px-6 py-3 text-center md:text-sm text-gray-50 dark:text-gray-300 uppercase tracking-wider">ID</th>
+                                    <th scope="col" class="px-6 py-3 text-center md:text-sm text-gray-50 dark:text-gray-300 uppercase tracking-wider">Producto</th>
+                                    <th scope="col" class="px-6 py-3 text-center md:text-sm text-gray-50 dark:text-gray-300 uppercase tracking-wider">Creación</th>
+                                    <th scope="col" class="px-6 py-3 text-center md:text-sm text-gray-50 dark:text-gray-300 uppercase tracking-wider">Actualización</th>
+                                    <th scope="col" class="px-6 py-3 text-center md:text-sm text-gray-50 dark:text-gray-300 uppercase tracking-wider">Acciones</th>
                                 </tr>
                                 </thead>
-                                <tbody>
+                                <tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
                                 @foreach ($productos as $producto)
-                                    <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
-                                        <td class="py-2 px-4 border-b">{{ $producto->id_producto }}</td>
-                                        <td class="py-2 px-4 border-b">{{ $producto->nombre }}</td>
-                                        <td class="py-2 px-4 border-b">{{ $producto->created_at->format('d/m/Y') }}</td>
-                                        <td class="py-2 px-4 border-b">{{ $producto->updated_at->format('d/m/Y') }}</td>
+                                    <tr class="hover:bg-gray-50 dark:hover:bg-gray-700">
+                                        <td class="px-6 py-4 border-b border-blue-gray-50 whitespace-nowrap text-center md:text-sm text-gray-900 dark:text-white">PROD0{{ $producto->id_producto }}</td>
+                                        <td class="px-6 border-b border-blue-gray-50 py-4 whitespace-nowrap md:text-sm text-gray-900 dark:text-white">{{ $producto->nombre }}</td>
+                                        <td class="px-6 border-b border-blue-gray-50 py-4 whitespace-nowrap text-center md:text-sm text-gray-500 dark:text-gray-300">{{ $producto->created_at->format('d/m/Y') }}</td>
+                                        <td class="px-6 border-b border-blue-gray-50 py-4 whitespace-nowrap text-center md:text-sm text-gray-500 dark:text-gray-300">{{ $producto->updated_at->format('d/m/Y') }}</td>
+                                        <td class="px-6 border-b border-blue-gray-50 py-4 whitespace-nowrap text-center text-gray-500 dark:text-gray-300">
+                                            <div class="flex justify-center items-center">
+                                                <a href="{{ route('productos.edit', $producto->id_producto) }}" class="py-2 px-4 rounded mr-2">
+                                                    <x-primary-button>
+                                                        {{ __('Modificar') }}
+                                                    </x-primary-button>
+                                                </a>
+                                                <form action="{{ route('productos.destroy', $producto->id_producto) }}" method="POST" class="inline">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <x-danger-button onclick="return confirm('¿Estás seguro de que quieres eliminar este {{ __('producto') }}?')">
+                                                        {{ __('Eliminar') }}
+                                                    </x-danger-button>
+                                                </form>
+                                            </div>
+                                        </td>
                                     </tr>
                                 @endforeach
                                 </tbody>
                             </table>
                         </div>
                     @endif
-
-                    {{-- Botón para agregar un nuevo producto --}}
-                    <div class="text-center mt-4">
-                        <a href="{{ route('productos.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Agregar Nuevo Producto
-                        </a>
+                    {{-- Paginación --}}
+                    <div class="mt-6">
+                        {{ $productos->links('vendor.pagination.tailwind') }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 </x-app-layout>
