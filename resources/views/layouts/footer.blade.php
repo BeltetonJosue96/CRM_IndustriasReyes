ccde0ad3b6d2944b19e393c7ebc94a1d8a4bbecc
BeltetonJosue96 <josuebelteton@icloud.com>
2024-09-07 22:59:10 -0600

    Implementación de footer.

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-white dark:bg-gray-800 shadow mt-8">
+    <div class="max-w-7xl mx-auto py-10 px-4 sm:px-6 lg:px-8 text-center">
+        <p class="text-gray-500 dark:text-gray-400 mb-2">
+            Copyright &copy; {{ date('Y') }}-2028 by Titan Solutions. Todos los derechos reservados.
+        </p>
+    </div>
+</footer>
