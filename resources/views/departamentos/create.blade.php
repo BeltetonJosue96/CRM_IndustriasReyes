a5a9e198bea6fbcbde202c205f9c0bb465bb2bdb
BeltetonJosue96 <josuebelteton@icloud.com>
2024-09-09 10:27:23 -0600

    Se modifican vistas de Departamento.

--- resources/views/departamentos/create.blade.php
+++ resources/views/departamentos/create.blade.php
@@ -9,56 +9,55 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="container mx-auto mt-8 bg-white dark:bg-gray-900 p-6 rounded-lg shadow-md">
-                        <h3 class="text-xl text-center font-semibold text-gray-800 dark:text-gray-200 mb-6">Registrar Nuevo Departamento</h3>
-                        @if ($errors->any())
-                            <div id="error-messages" class="text-black dark:text-gray-200 rounded-lg p-4 mb-4">
-                                <ul class="text-center">
-                                    @foreach ($errors->all() as $error)
-                                        <li class="text-xl font-bold">{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-                        <script>
-                            setTimeout(function() {
-                                var errorMessages = document.getElementById('error-messages');
-                                if (errorMessages) {
-                                    errorMessages.style.display = 'none';
-                                }
-                            }, 5000);
-                        </script>
-                        @if (session('success'))
-                            <div class="px-4 py-3 rounded mb-4">
-                                {{ session('success') }}
-                            </div>
-                        @endif
+                    <h2 class="text-xl text-center font-semibold text-gray-800 dark:text-gray-200 mb-6">Registrar Nuevo Departamento</h2>
+                    @if ($errors->any())
+                        <div id="error-messages" class="text-black dark:text-gray-200 rounded-lg p-4 mb-4">
+                            <ul class="text-center">
+                                @foreach ($errors->all() as $error)
+                                    <li class="text-xl font-bold">{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <script>
+                        setTimeout(function() {
+                            var errorMessages = document.getElementById('error-messages');
+                            if (errorMessages) {
+                                errorMessages.style.display = 'none';
+                            }
+                        }, 5000);
+                    </script>
+                    @if (session('success'))
+                        <div class="px-4 py-3 rounded mb-4">
+                            {{ session('success') }}
+                        </div>
+                    @endif
 
-                        <form action="{{ route('departamentos.store') }}" method="POST">
-                            @csrf
+                    <form action="{{ route('departamentos.store') }}" method="POST">
+                        @csrf
 
-                            <div class="grid grid-cols-1 gap-4 mb-6">
-                                <div class="flex items-center">
-                                    <label for="nombre" class="block text-lg font-medium text-gray-700 dark:text-gray-300 pr-4">Nombre del departamento</label>
-                                    <input type="text" name="nombre" id="nombre" class="w-full p-3 border border-gray-300 rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:text-white" required>
-                                </div>
+                        <div class="grid grid-cols-1 gap-4 mb-6">
+                            <div class="flex items-center">
+                                <label for="nombre" class="block text-lg font-medium text-gray-700 dark:text-gray-300 pr-4">Nombre del departamento</label>
+                                <input type="text" name="nombre" id="nombre" class="w-full p-3 border border-gray-300 rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:text-white" required>
                             </div>
-
-                            <div class="flex justify-center space-x-4 mt-6">
-                                <x-primary-button class="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow">
-                                    Agregar
-                                </x-primary-button>
-                            </div>
-                        </form>
+                        </div>
 
                         <div class="flex justify-center space-x-4 mt-6">
-                            <a href="{{ route('departamentos.index') }}">
-                                <x-danger-button class="px-6 py-3 bg-red-600 hover:bg-red-700 text-white rounded-lg shadow">
-                                    Cancelar
-                                </x-danger-button>
-                            </a>
+                            <x-primary-button class="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow">
+                                Agregar
+                            </x-primary-button>
                         </div>
+                    </form>
+
+                    <div class="flex justify-center space-x-4 mt-6">
+                        <a href="{{ route('departamentos.index') }}">
+                            <x-danger-button class="px-6 py-3 bg-red-600 hover:bg-red-700 text-white rounded-lg shadow">
+                                Cancelar
+                            </x-danger-button>
+                        </a>
                     </div>
+
                 </div>
             </div>
         </div>
--- resources/views/departamentos/index.blade.php
+++ resources/views/departamentos/index.blade.php
@@ -7,56 +7,53 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="text-center mt-6">
-                        <a href="{{ route('departamentos.create') }}">
-                            <x-primary-button class="ms-3">
-                                {{ __('Agregar nuevo departamento') }}
-                            </x-primary-button>
-                        </a>
-                    </div>
-                    <div class="mt-6 flex justify-between items-center">
-                        <h3 class="justify-start text-xl text-gray-50 dark:text-gray-400">Lista de departamentos</h3>
-                        <form action="{{ route('departamentos.index') }}" method="GET" class="flex">
-                            <input type="text" name="search" value="{{ request('search') }}" placeholder="Buscar departamentos..." class="px-4 py-2 border rounded-md dark:bg-gray-700 dark:text-white dark:border-gray-600">
-                            <x-primary-button class="ms-3">
-                                Buscar/Limpiar
-                            </x-primary-button>
-                        </form>
-                    </div>
-                    @if($departamentos->isEmpty())
-                        <p class="text-center text-gray-500 dark:text-gray-400">Sin coincidencias, no hay departamentos disponibles en este momento.</p>
-                    @else
-                        <div class="mt-6 relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
-                            <table class="w-full table-auto min-w-max divide-y divide-gray-200 dark:divide-gray-700">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <!-- Comienza el contenido -->
+                        <div class="text-center">
+                            <a href="{{ route('departamentos.create') }}">
+                                <x-primary-button class="ms-3">
+                                    {{ __('Agregar nuevo departamento') }}
+                                </x-primary-button>
+                            </a>
+                            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mt-6">Departamentos registrados</h2>
+                        </div>
+                        <!-- Cuadro de búsqueda centrado -->
+                        <div class="flex flex-col items-center mt-2">
+                            <form action="{{ route('departamentos.index') }}" method="GET" class="flex items-center space-x-2">
+                                <input type="text" name="search" value="{{ request('search') }}" placeholder="Buscar departamentos..." class="px-4 py-2 border rounded-md dark:bg-gray-700 dark:text-white dark:border-gray-600">
+                                <x-primary-button class="ms-1">
+                                    🔍
+                                </x-primary-button>
+                            </form>
+                        </div>
+
+                        @if($departamentos->isEmpty())
+                            <p class="text-center text-gray-500 dark:text-gray-400">Sin coincidencias, no hay departamentos disponibles en este momento.</p>
+                        @else
+                            <table class="mt-6 w-full table-auto items-center">
                                 <thead class="bg-gray-50 dark:bg-gray-700">
                                 <tr>
-                                    <th scope="col" class="px-6 py-3 text-center md:text-sm text-gray-50 dark:text-gray-300 uppercase tracking-wider">Departamento</th>
-                                    <th scope="col" class="px-6 py-3 text-center md:text-sm text-gray-50 dark:text-gray-300 uppercase tracking-wider">Creación</th>
-                                    <th scope="col" class="px-6 py-3 text-center md:text-sm text-gray-50 dark:text-gray-300 uppercase tracking-wider">Última modificación</th>
-                                    <th scope="col" class="px-6 py-3 text-center md:text-sm text-gray-50 dark:text-gray-300 uppercase tracking-wider">Acciones</th>
+                                    <th class="px-4 py-2">Departamento</th>
+                                    <th class="px-4 py-2">Acciones</th>
                                 </tr>
                                 </thead>
                                 <tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
                                 @foreach ($departamentos as $departamento)
                                     <tr class="hover:bg-gray-50 dark:hover:bg-gray-700">
-                                        <td class="px-6 border-b border-blue-gray-50 py-4 whitespace-nowrap md:text-sm text-gray-900 dark:text-white">{{ $departamento->nombre }}</td>
-                                        <td class="px-6 border-b border-blue-gray-50 py-4 whitespace-nowrap text-center md:text-sm text-gray-500 dark:text-gray-300">{{ $departamento->created_at->format('d/m/Y') }}</td>
-                                        <td class="px-6 border-b border-blue-gray-50 py-4 whitespace-nowrap text-center md:text-sm text-gray-500 dark:text-gray-300">{{ $departamento->updated_at->format('d/m/Y') }}</td>
-                                        <td class="px-6 border-b border-blue-gray-50 py-4 whitespace-nowrap text-center text-gray-500 dark:text-gray-300">
-                                            <div class="flex justify-center items-center">
-                                                <a href="{{ route('departamentos.edit', $departamento->hashed_id ) }}" class="py-2 px-4 rounded mr-2">
-                                                    <x-primary-button>
-                                                        {{ __('Modificar') }}
-                                                    </x-primary-button>
+                                        <td class="px-4 py-2">{{ $departamento->nombre }}</td>
+                                        <td class="px-4 py-2">
+                                            <div class="flex justify-center items-center space-x-2">
+                                                <a href="{{ route('departamentos.edit', $departamento->hashed_id ) }}" class="py-2 px-4 rounded bg-blue-500 text-white hover:bg-blue-700">
+                                                    ✍️
                                                 </a>
                                                 <form action="{{ route('departamentos.destroy', $departamento->hashed_id ) }}" method="POST" class="inline">
                                                     @csrf
                                                     @method('DELETE')
-                                                    <x-danger-button onclick="return confirm('¿Estás seguro de que quieres eliminar este {{ __('departamento') }}?')">
-                                                        {{ __('Eliminar') }}
-                                                    </x-danger-button>
+                                                    <button type="submit" onclick="return confirm('¿Estás seguro de que quieres eliminar este departamento?')" class="py-2 px-4 rounded bg-red-500 text-white hover:bg-red-700">
+                                                        🗑️
+                                                    </button>
                                                 </form>
                                             </div>
                                         </td>
@@ -64,15 +61,14 @@
                                 @endforeach
                                 </tbody>
                             </table>
+                        @endif
+                        {{-- Paginación --}}
+                        <div class="mt-6">
+                            {{ $departamentos->links('vendor.pagination.tailwind') }}
                         </div>
-                    @endif
-                    {{-- Paginación --}}
-                    <div class="mt-6">
-                        {{ $departamentos->links('vendor.pagination.tailwind') }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 </x-app-layout>
