168443b67c76e8386e1e33588c9210868bc43e5f
BeltetonJosue96 <josuebelteton@icloud.com>
2024-09-15 21:15:34 -0600

    Se mejora vistas de tableros principal y de configuraciones.

--- resources/views/config.blade.php
+++ resources/views/config.blade.php
@@ -9,36 +9,59 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <a href="{{ route('productos.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
-                        <img src="https://cdn-icons-png.freepik.com/256/2702/2702154.png" alt="Productos" class="w-20 h-20 mr-2">
-                        <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
-                            Catálogo de Productos
-                        </span>
-                    </a>
-                    <a href="{{ route('planes.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
-                        <img src="https://cdn-icons-png.freepik.com/256/4233/4233834.png" alt="Planes" class="w-20 h-20 mr-2">
-                        <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
-                            Planes de Mantenimiento
-                        </span>
-                    </a>
-                    <a href="{{ route('empresas.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
-                        <img src="https://cdn-icons-png.freepik.com/256/6082/6082589.png" alt="Empresas" class="w-20 h-20 mr-2">
-                        <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
-                            Empresas
-                        </span>
-                    </a>
-                    <a href="{{ route('estados.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
-                        <img src="https://cdn-icons-png.freepik.com/256/8558/8558628.png" alt="Estados" class="w-20 h-20 mr-2">
-                        <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
-                            Estados
-                        </span>
-                    </a>
-                    <a href="{{ route('departamentos.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
-                        <img src="https://cdn-icons-png.freepik.com/256/2060/2060593.png" alt="Departamentos" class="w-20 h-20 mr-2">
-                        <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
-                            Departamentos de Guatemala
-                        </span>
-                    </a>
+                    <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">
+                        Opciones de configuración
+                    </h1>
+                    <div class="grid grid-cols-2 gap-4 mt-6 place-items-center">
+                        <div class="w-full flex justify-center">
+                            <a href="{{ route('productos.index') }}" class="w-40 p-4 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+                                <img src="https://cdn-icons-png.freepik.com/256/2702/2702154.png" alt="Productos" class="w-20 h-20 mb-2">
+                                <span class="text-center text-lg font-semibold text-gray-800 dark:text-gray-200">
+                                    Catálogo de Productos
+                                </span>
+                            </a>
+                        </div>
+                        <div class="w-full flex justify-center">
+                            <a href="{{ route('planes.index') }}" class="w-40 p-4 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+                                <img src="https://cdn-icons-png.freepik.com/256/4233/4233834.png" alt="Planes" class="w-20 h-20 mb-2">
+                                <span class="text-center text-lg font-semibold text-gray-800 dark:text-gray-200">
+                                    Planes de Mantenimiento
+                                </span>
+                            </a>
+                        </div>
+                        <div class="w-full flex justify-center">
+                            <a href="{{ route('empresas.index') }}" class="w-40 p-4 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+                                <img src="https://cdn-icons-png.freepik.com/256/6082/6082589.png" alt="Empresas" class="w-20 h-20 mb-2">
+                                <span class="text-center text-lg font-semibold text-gray-800 dark:text-gray-200">
+                                    Empresas
+                                </span>
+                            </a>
+                        </div>
+                        <div class="w-full flex justify-center">
+                            <a href="{{ route('estados.index') }}" class="w-40 p-4 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+                                <img src="https://cdn-icons-png.freepik.com/256/8558/8558628.png" alt="Estados" class="w-20 h-20 mb-2">
+                                <span class="text-center text-lg font-semibold text-gray-800 dark:text-gray-200">
+                                    Estados
+                                </span>
+                            </a>
+                        </div>
+                        <div class="w-full flex justify-center">
+                            <a href="{{ route('departamentos.index') }}" class="w-40 p-4 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+                                <img src="https://cdn-icons-png.freepik.com/256/2060/2060593.png" alt="Departamentos" class="w-20 h-20 mb-2">
+                                <span class="text-center text-lg font-semibold text-gray-800 dark:text-gray-200">
+                                    Departamentos de Guatemala
+                                </span>
+                            </a>
+                        </div>
+                        <div class="w-full flex justify-center">
+                            <a href="{{ route('dashboard') }}" class="w-40 p-4 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+                                <img src="https://cdn-icons-png.freepik.com/256/11997/11997336.png" alt="Regresar" class="w-20 h-20 mb-2">
+                                <span class="text-center text-lg font-semibold text-gray-800 dark:text-gray-200">
+                                    Regresar
+                                </span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,38 +9,40 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h3 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">
+                    <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">
                         Bienvenido(a)
-                    </h3>
-                    <div class="grid grid-cols-2 gap-8 mt-6">
-                        <a href="{{ route('clientes.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
-                            <img src="https://cdn-icons-png.freepik.com/256/2194/2194222.png" alt="Clientes" class="w-20 h-20 mb-2">
-                            <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">Clientes</span>
-                        </a>
-                        <a href="{{ route('checklist.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
-                            <img src="https://cdn-icons-png.freepik.com/256/2666/2666436.png" alt="Checklist" class="w-20 h-20 mr-2">
-                            <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
-                                Checklist Posventa
-                            </span>
-                        </a>
-                        <a href="{{ route('ventas.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
-                            <img src="https://cdn-icons-png.freepik.com/256/781/781760.png" alt="Checklist" class="w-20 h-20 mr-2">
-                            <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
-                                Ventas
-                            </span>
-                        </a>
-                        <a href="{{ route('historial.index') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
-                            <img src="https://cdn-icons-png.freepik.com/256/2936/2936725.png" alt="Checklist" class="w-20 h-20 mr-2">
-                            <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
-                                Reportes
-                            </span>
-                        </a>
-                        <a href="{{ route('config') }}" class="box-border h-40 w-40 p-4 border-4 flex flex-col items-center justify-center">
-                            <img src="https://cdn-icons-png.freepik.com/256/3315/3315581.png" alt="Configuracion" class="w-20 h-20 mr-2">
-                            <span class="text-center text-xl font-semibold text-gray-800 dark:text-gray-200">
-                                Configuración
-                            </span>
-                        </a>
+                    </h1>
+                    <div class="grid grid-cols-2 gap-4 mt-6 place-items-center">
+                        <div class="w-full flex justify-center">
+                            <a href="{{ route('clientes.index') }}" class="w-40 p-4 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+                                <img src="https://cdn-icons-png.freepik.com/256/2194/2194222.png" alt="Clientes" class="w-20 h-20 mb-2">
+                                <span class="text-center text-lg font-semibold text-gray-800 dark:text-gray-200">Clientes</span>
+                            </a>
+                        </div>
+                        <div class="w-full flex justify-center">
+                            <a href="{{ route('checklist.index') }}" class="w-40 p-4 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+                                <img src="https://cdn-icons-png.freepik.com/256/2666/2666436.png" alt="Checklist" class="w-20 h-20 mr-2">
+                                <span class="text-center text-lg font-semibold text-gray-800 dark:text-gray-200">Checklist Posventa</span>
+                            </a>
+                        </div>
+                        <div class="w-full flex justify-center">
+                            <a href="{{ route('ventas.index') }}" class="w-40 p-4 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+                                <img src="https://cdn-icons-png.freepik.com/256/781/781760.png" alt="Ventas" class="w-20 h-20 mr-2">
+                                <span class="text-center text-lg font-semibold text-gray-800 dark:text-gray-200">Ventas</span>
+                            </a>
+                        </div>
+                        <div class="w-full flex justify-center">
+                            <a href="{{ route('historial.index') }}" class="w-40 p-4 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+                                <img src="https://cdn-icons-png.freepik.com/256/2936/2936725.png" alt="Reportes" class="w-20 h-20 mr-2">
+                                <span class="text-center text-lg font-semibold text-gray-800 dark:text-gray-200">Reportes</span>
+                            </a>
+                        </div>
+                        <div class="w-full flex justify-center">
+                            <a href="{{ route('config') }}" class="w-40 p-4 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+                                <img src="https://cdn-icons-png.freepik.com/256/3315/3315581.png" alt="Configuracion" class="w-20 h-20 mr-2">
+                                <span class="text-center text-lg font-semibold text-gray-800 dark:text-gray-200">Configuración</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
